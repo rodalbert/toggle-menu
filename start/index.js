99769ca6d15734c66f7b72cf015969e17bad423f
Marco Bruno <marco.bruno.br@gmail.com>
2019-07-06 12:11:58 +0100

    Fizemos o role do menu

--- start/index.js
+++ start/index.js
@@ -1,1 +1,10 @@
 /* YOUR CODE HERE */
+const $toggleMenu = document.querySelector(".toggle-menu");
+const $menuDrawer = document.querySelector(".menu-drawer");
+
+const handleClick = () => {
+  $toggleMenu.classList.toggle("active");
+  $menuDrawer.classList.toggle("open");
+};
+
+$toggleMenu.addEventListener("click", handleClick);
